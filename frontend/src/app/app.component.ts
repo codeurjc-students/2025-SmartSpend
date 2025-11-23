1c89abb4dae606f3014759c6bfca5e29fc61f648
Juanmabm24 <148887174+Juanmabm24@users.noreply.github.com>
2025-11-23 08:09:41 +0100

    Feature/nav bar (#36)
    * feat(nav-bar): Add navigation bar component with styling and links
    
    * fix(nav-bar): Adjust navbar height and body padding for better layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 import { LoginRegisterComponent } from './components/auth-components/login-register/login-register.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    NavBarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
